d448fd6aaf437a35a8f3e5db9f32c63281998d3d
Simon Milord <simon.milord@gmail.com>
2022-04-12 11:07:58 -0400

    added basic hero

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './hero.scss';
+import Image from "../../assets/placeholder.svg";
 
 function hero(props) {
   return (
     <div className='hero'>
-      <h1>hero</h1>
+      <div className='hero__text'>
+        <h1>Hello!</h1>
+        <h2>I'm Simon, a web developer</h2>
+      </div>
+      <div className='hero__img-box'>
+        <img src={Image} alt="illustration of me" className='hero__img'></img>
+      </div>
     </div>
   );
 }
