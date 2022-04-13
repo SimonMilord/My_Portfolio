b037b57839222a3d81f5972a5c9ead832f078e83
Simon Milord <simon.milord@gmail.com>
2022-04-13 09:48:20 -0400

    added form submission email feature and basic validation

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -1,21 +1,41 @@
-import React from "react";
 import "./contact.scss";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-function contact(props) {
+export default function Contact(props) {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    const field = e.target;
+    if (field.user_name.value && field.user_email.value && field.message.value !== "") {
+      emailjs.sendForm('service_egkh5yr', 'template_7esbhub', form.current, 'x4gZeZxQ6lq4l--CY')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset();
+    } else {
+      alert("please fill all the fields");
+    }
+    e.preventDefault();
+  };
+
   return (
     <div className="contact">
       <h2 className="contact-title">Get in touch</h2>
-      <form className="emailForm">
+      <form className="emailForm" ref={form} onSubmit={sendEmail}>
         <input
           type="text"
           className="emailForm__input emailForm__input--name"
-          name="name"
+          name="user_name"
           placeholder="Full name*"
         ></input>
         <input
           type="email"
           className="emailForm__input emailForm__input--email"
-          name="email"
+          name="user_email"
           placeholder="Email*"
         ></input>
         <textarea
@@ -23,10 +43,9 @@ function contact(props) {
           name="message"
           placeholder="Message*"
         ></textarea>
-        <button type="submit" className="emailForm__btn">SEND</button>
+        <button type="submit" value="Send" className="emailForm__btn">SEND</button>
       </form>
     </div>
   );
 }
 
-export default contact;
