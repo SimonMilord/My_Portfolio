e0caa8db1d7071a584333820a5c58fda08fc4996
Simon Milord <simon.milord@gmail.com>
2022-04-14 17:13:03 -0400

    pre styling commit

--- src/components/projects.js
+++ src/components/projects.js
@@ -19,24 +19,6 @@ const stonkers = Project(
   "Stonkers is a fully responsive web application that allows users to look up quotes, charts, news and stats of stocks. It also allows them to add stocks to their watchlist. It was built using the MERN stack, Chart.JS and JWT. It features databases, server-side authentication and the use of API data. "
 );
 
-const stonkersa = Project(
-  "Stonkers",
-  stonkersImg,
-  2022,
-  "https://stonkers.netlify.app/",
-  "https://github.com/SimonMilord/Stonkers",
-  "Stonkers is a fully responsive web application that allows users to look up quotes, charts, news and stats of stocks. It also allows them to add stocks to their watchlist. It was built using the MERN stack, Chart.JS and JWT. It features databases, server-side authentication and the use of API data. "
-);
-
-const stonkersb = Project(
-  "Stonkers",
-  stonkersImg,
-  2022,
-  "https://stonkers.netlify.app/",
-  "https://github.com/SimonMilord/Stonkers",
-  "Stonkers is a fully responsive web application that allows users to look up quotes, charts, news and stats of stocks. It also allows them to add stocks to their watchlist. It was built using the MERN stack, Chart.JS and JWT. It features databases, server-side authentication and the use of API data. "
-);
-
-const projects = [stonkers, stonkersa, stonkersb];
+const projects = [stonkers];
 
 export default projects;
