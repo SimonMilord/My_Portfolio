65958dca5c64515cdeae9b783fe45d96c12c20b4
Simon Milord <simon.milord@gmail.com>
2022-04-16 00:34:01 -0400

    added to project desc / test for post-deployment updates

--- src/components/projects.js
+++ src/components/projects.js
@@ -16,7 +16,10 @@ const stonkers = Project(
   2022,
   "https://stonkers.netlify.app/",
   "https://github.com/SimonMilord/Stonkers",
-  "Stonkers is a fully responsive web application that allows users to look up quotes, charts, news and stats of stocks. It also allows them to add stocks to their watchlist. It was built using the MERN stack, Chart.JS and JWT. It features databases, server-side authentication and the use of API data. "
+  `Stonkers is a fully responsive web application that allows users to look up quotes, charts, news and stats of stocks.
+  It also allows them to add stocks to their watchlist. It was built using the MERN stack, Chart.JS and JWT.
+  It features databases, server-side authentication and the use of API data. The app seeks to provide beginners and new investors
+  a simple to use interface to help them keep an eye on their investments. `
 );
 
 const projects = [stonkers];
