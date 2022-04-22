deffeb4a09672f0bb4b5890aa201f853078eec98
Simon Milord <simon.milord@gmail.com>
2022-04-22 13:08:08 -0400

    added instock project and fixed display of project section removing 100vh

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,4 +1,5 @@
 import stonkersImg from "../assets/images/stonkers-homepage.png";
+import instockImg from "../assets/images/instock-warehouses.png";
 
 const Project = (name, img, year, demo, code, desc) => {
   const getName = () => name;
@@ -22,6 +23,18 @@ const stonkers = Project(
   a simple to use interface to help them keep an eye on their investments. `
 );
 
-const projects = [stonkers];
+const instock = Project(
+  "InStock",
+  instockImg,
+  2022,
+  "https://instock-demo.netlify.app/",
+  "https://github.com/SimonMilord/instock_client",
+  `InStock is a responsive inventory management application that allows users to keep track of inventories spread across multiple warehouses.
+   It was built as a team project for the Brainstation program where we got to collaborate in an agile development environment using Jira.
+   The app itself was built with React and SASS on the frontend and Node and Express on the backend. My contributions range from backend routing,
+   to React logic, to building some React components for the frontend including styling with SASS.`
+);
+
+const projects = [stonkers, instock];
 
 export default projects;
