dec63b3771138c90b3fe301964af479f5c5e4401
Simon Milord <simon.milord@gmail.com>
2022-04-29 10:21:34 -0400

    added resume button on mobile and desktop in hero section

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -5,26 +5,36 @@ import Illustration from "../../assets/images/illustrationHero.svg";
 function Hero(props) {
   return (
     <div className="hero">
-      <div className="hero__text">
-        <span className="hero__intro">Hi, my name is</span>
-        <h1 className="hero__title">Simon Milord</h1>
-        <h2 className="hero__subtitle">Web developer</h2>
-        <div className="hero__desc">
-          <p className="hero__desc-content">
-            {" "}
-            I'm a developer from Canada with a background in finance and a keen
-            interest in Fintech and E-Commerce. I'm also a CFA charter holder
-            and was an investment advisor in my past life!
-          </p>
+      <div className="hero-top">
+        <div className="hero__text">
+          <span className="hero__intro">Hi, my name is</span>
+          <h1 className="hero__title">Simon Milord</h1>
+          <h2 className="hero__subtitle">Web developer</h2>
+          <div className="hero__desc">
+            <p className="hero__desc-content">
+              {" "}
+              I'm a developer from Canada with a background in finance and a
+              keen interest in Fintech. I'm also a CFA charter holder and was an
+              investment advisor in my past life!
+            </p>
+          </div>
+          <div className="hero__btn-box">
+            <a className="btn hero__btn--desktop" href="/resume.pdf">
+              RESUME
+            </a>
+          </div>
+        </div>
+        <div className="hero__img-box">
+          <img
+            src={Illustration}
+            alt="illustration of me"
+            className="hero__img"
+          ></img>
         </div>
       </div>
-      <div className="hero__img-box">
-        <img
-          src={Illustration}
-          alt="illustration of me"
-          className="hero__img"
-        ></img>
-      </div>
+      <a className="btn hero__btn--mobile" href="/resume.pdf">
+        RESUME
+      </a>
     </div>
   );
 }
