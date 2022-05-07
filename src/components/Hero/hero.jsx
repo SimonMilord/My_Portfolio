175b067e200fb67f96e10dc201ebe3381f345235
Simon Milord <simon.milord@gmail.com>
2022-05-07 17:11:56 +0800

    modified text in hero and removed CV icon in about, fixed about for darkmode mobile

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -7,7 +7,7 @@ function Hero(props) {
     <div className="hero">
       <div className="hero-top">
         <div className="hero__text">
-          <span className="hero__intro">Hi, my name is</span>
+          <span className="hero__intro">Hi, I'm</span>
           <h1 className="hero__title">Simon Milord</h1>
           <h2 className="hero__subtitle">Web developer</h2>
           <div className="hero__desc">
